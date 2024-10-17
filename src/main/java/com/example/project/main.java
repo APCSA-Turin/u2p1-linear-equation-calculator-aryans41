8b05227c69f27250b6339da7f0d5cbc07625061d
aryans41 <aryans41@nycstudents.net>
2024-10-17 22:23:03 +0000

    second commit

--- src/main/java/com/example/project/main.java
+++
@@ -1,7 +0,0 @@
-package com.example.project;
-public class main {
-    public static void main(String[] args) {
-        LinearCalculator c = new LinearCalculator("(0,0)","(5,68)");
-        System.out.println(c.printInfo());
-    }
-}
